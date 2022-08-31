8100d0627b3b73a017276db8f3bb795bd833c48c
Mathieu CAROFF <mathieu.caroff@free.fr>
2022-08-31 22:48:36 +0200

    level drawing

--- src/level/importerObject.ts
+++ src/level/importerObject.ts
@@ -1,5 +1,31 @@
 import { pathGroup } from './levelImporter'
 
+export type Direction = 'top' | 'bottom' | 'left' | 'right'
+
+export type GridPosition = {
+  column: number
+  row: number
+}
+
+export interface LevelObject {
+  stations: StationEntry[]
+  tracks: TrackEntry[]
+}
+
+export interface StationEntry extends GridPosition {
+  type: 'start' | 'normal'
+  exit: Direction
+  color: string
+}
+
+export interface TrackEntry extends GridPosition {
+  type: 'straight' | 'curved'
+  start: Direction
+  end1: Direction
+  end2: Direction
+  color: string
+}
+
 export interface ImporterObject {
   [levelNumber: string]: {
     [alternativeNumber: string]: typeof pathGroup[keyof typeof pathGroup]
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as pixi from 'pixi.js'
-import { road, roadTurn, SQUARE_WIDTH, station, train } from './graphics/graphics'
-import { importerObject } from './level/importerObject'
+import * as graphics from './graphics/graphics'
+import { Direction, GridPosition, importerObject, TrackEntry } from './level/importerObject'
 
 let type = 'WebGL'
 if (!pixi.utils.isWebGLSupported()) {
@@ -26,6 +26,47 @@ let randomPick = <T>(array: T[]) => {
   return array[Math.floor(Math.random() * array.length)]
 }
 
+let aligned = (a: Direction, b: Direction) => {
+  let [c, d] = [a, b].sort()
+  return (c === 'bottom' && d === 'top') || (c === 'left' && d === 'right')
+}
+
+let setPosition = (g: pixi.Container, position: GridPosition) => {
+  g.x += graphics.SQUARE_WIDTH * position.column
+  g.y += graphics.SQUARE_WIDTH * position.row
+  return g
+}
+
+let simpleTrack = (start: Direction, end: Direction) => {
+  if (start === end) {
+    throw new Error(`encountered equal start and end (${start}) in some level file`)
+  }
+  if (aligned(start, end)) {
+    let result = graphics.road()
+    if ([start, end].includes('top')) {
+      result.rotation = Math.PI / 2
+      result.x += graphics.SQUARE_WIDTH
+    }
+    return result
+  } else {
+    let turn = graphics.roadTurn()
+    let [c, d] = [start, end].sort()
+    if (c + d === 'lefttop') {
+    } else if (c + d === 'righttop') {
+      turn.rotation = Math.PI / 2
+      turn.x += graphics.SQUARE_WIDTH
+    } else if (c + d === 'bottomright') {
+      turn.rotation = Math.PI
+      turn.x += graphics.SQUARE_WIDTH
+      turn.y += graphics.SQUARE_WIDTH
+    } else {
+      turn.rotation = -Math.PI / 2
+      turn.y += graphics.SQUARE_WIDTH
+    }
+    return turn
+  }
+}
+
 let main = async () => {
   let [levelNumber, alternativeImporterObject] = randomPick(Object.entries(importerObject))
   let [alternativeNumber, importer] = randomPick(Object.entries(alternativeImporterObject))
@@ -35,5 +76,28 @@ let main = async () => {
   let levelContent = await importer()
 
   console.log('levelContent', levelContent)
+
+  // draw tracks
+  levelContent.tracks.forEach((track) => {
+    let result = new pixi.Container()
+
+    let g = simpleTrack(track.start, track.end1)
+    setPosition(g, track)
+    result.addChild(g)
+    if (track.switch === 'true') {
+      g = simpleTrack(track.start, track.end2)
+      setPosition(g, track)
+      result.addChild(g)
+    }
+    app.stage.addChild(result)
+  })
+
+  // draw stations
+  levelContent.stations.forEach((entry) => {
+    let g = graphics.station(entry.color)
+    setPosition(g, entry)
+    app.stage.addChild(g)
+  })
 }
+
 main()
