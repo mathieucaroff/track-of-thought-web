0d57c84ea34df8d8c693eb826c7bd4f710714b60
Mathieu CAROFF <mathieu.caroff@free.fr>
2022-08-31 23:40:35 +0200

    move level files to levelData/

--- src/level/generate-level-importer.js
+++ src/level/generate-level-importer.js
@@ -1,13 +1,13 @@
 const { readdirSync, writeFileSync } = require('fs')
 const { resolve } = require('path')
 
-let pathGroup = readdirSync(resolve(__dirname, 'levelFile'), { withFileTypes: true })
+let pathGroup = readdirSync(resolve(__dirname, 'levelData'), { withFileTypes: true })
   .filter((entry) => entry.isFile && entry.name.split('.')[1] === 'json')
   .map((entry) => {
     let [stem, _extension] = entry.name.split('.')
     let [_, major, minor] = stem.split('-')
     let escaped = entry.name.replace(/\\/g, '\\\\').replace(/'/g, "\\'")
-    return `  '${major.padStart(2, '0')}-${minor}': () => import('./levelFile/${escaped}'),`
+    return `  '${major.padStart(2, '0')}-${minor}': () => import('./levelData/${escaped}'),`
   })
   .sort()
   .join('\n')
